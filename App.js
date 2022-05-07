b02473ad2452a1bae3c5c737524a9ebb44343405
luisbull <uk.luistoro@gmail.com>
2022-05-07 23:22:10 +0100

    Moved code from App.js Creatation of styles.js file to keep CSS organised in one file. Creation of Login.js (screen) file with: logo, email and password inputs

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from 'react';
+
+// screens
+import Login from "./screens/Login";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>FLOWERS APP</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Login/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
